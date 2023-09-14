e45e6ce3e9e23ba94afcfd1a9d916e838355db65
antcorper <99472871+antcorper@users.noreply.github.com>
2023-09-14 19:56:17 +0200

    Add files via upload

--- incluirMasElementos.py
+++ incluirMasElementos.py
@@ -1,4 +1,6 @@
+"Creo lista"
 finca_datos = ["poligono", "parcela", "subparcela"]
 print(finca_datos)
+"añado elementos nuevos a la lista"
 finca_datos = finca_datos + ["superficie", "uso"]
-print(finca_datos)
+print(finca_datos[3])
